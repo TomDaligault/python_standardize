cb72b98b08a7198e008fb4b9cf13eae232fd21a7
Daligault <tdaligau@slac.stanford.edu>
2025-11-19 23:38:31 -0800

    start of week

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from PyQt5.QtWidgets import QApplication
 import sys
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	magnet_interface = EpicsMagnetInterface()
-	ui = MainUI()
-	ui.setStyleSheet(styles.dark_mode)
-	presenter = Presenter(magnet_interface, ui)
-	ui.show()
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    magnet_interface = EpicsMagnetInterface()
+    ui = MainUI()
+    ui.setStyleSheet(styles.dark_mode)
+    presenter = Presenter(magnet_interface, ui)
+    ui.show()
+    sys.exit(app.exec_())
--- presenter/presenter.py
+++ presenter/presenter.py
@@ -1,35 +1,31 @@
 class Presenter:
-	def __init__(self, magnet_interface, ui):
-		self.magnet_interface = magnet_interface
-		self.ui = ui
+    def __init__(self, magnet_interface, ui):
+        self.magnet_interface = magnet_interface
+        self.ui = ui
 
-		# hxr_magnets_by_health = self.magnet_interface.get_magnets_by_health('HXR')
-		# sxr_magnets_by_health = self.magnet_interface.get_magnets_by_health('SXR')
-		
-		hxr_magnets_by_health = {'healthy': 
-											{'BEND:BSYH:140': 'Not Stdz\'d',
-											 'BEND:LTUH:210': 'Not Stdz\'d',
-											 'QUAD:LTUH:480': 'Out-of-Tol', 
-											 'QUAD:DMPH:740': 'good', 
-											 'QUAD:DMPH:741': 'BCON Warning'},
-								 'nonhealthy': 
-								 			{'BEND:LTUH:240': 'Offline',
-											 'QUAD:DMPH:720': 'Offline'}}
-		sxr_magnets_by_health ={'healthy': 
-											{}, 
-								'nonhealthy': 
-											{'Q1': 'REALLY bad'}}
+        hxr_magnets_by_health = self.magnet_interface.get_magnets_by_health('HXR')
+        sxr_magnets_by_health = self.magnet_interface.get_magnets_by_health('SXR')
+      
+        self.ui.make_new_standarize_tab("HXR", *hxr_magnets_by_health.values())
+        self.ui.make_new_standarize_tab("SXR", *sxr_magnets_by_health.values())
+        self.ui.standardize_confirmed.connect(self.start_standardize)
 
-		self.ui.make_new_standarize_tab("HXR", *hxr_magnets_by_health.values())
-		self.ui.make_new_standarize_tab("SXR", *sxr_magnets_by_health.values())
-		self.ui.standardize_confirmed.connect(self.start_standardize)
+    def start_standardize(self, magnets):
+    		self.shutter_beams()
+            self.magnet_interface.standardize(magnets)
+            self.launch_striptool(magnets)
+            self.ui.close()
 
-	def start_standardize(self, magnets):
-			self.magnet_interface.standardize(magnets)
-			self.launch_striptool(magnets)
-			self.ui.close()
+    def shutter_beams(self):
+    	"""
+    	Not implemented. Always shutter both NC and SC feels like the safest move here.
+    	"""
+    	pass
 
-	def launch_striptool(self, beamline):
-		print("I don't know how to launch Michael's new striptool from python.")
+    def launch_striptool(self):
+    	"""
+		Not implemented. Need to see if Michael's striptool + archiver can be launched programatically.
+    	"""
+        pass
 
 
